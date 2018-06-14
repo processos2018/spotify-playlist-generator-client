a57ff766d50f1a18836d530a4ba6245764977c41
Giordano Rodrigues <giordano2dcc@gmail.com>
2018-06-14 12:22:08 -0300

    Criação da pagina de customização

---
+++ src/components/pages/Customizer.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+section#customizer
+  h1 Escolha os parâmetros desejados para gerar sua playlist
+  form(@submit.prevent="redirect")
+    .input
+      p acousticness
+      input(type="range")#acousticness
+    .input
+      p danceability
+      input(type="range")#danceability
+    .input
+      p energy
+      input(type="range")#energy
+    .input
+      p instrumentalness
+      input(type="range")#instrumentalness
+    .input
+      p liveness
+      input(type="range")#liveness
+    .input
+      p loudness
+      input(type="range")#loudness
+    .input
+      p speechiness
+      input(type="range")#speechiness
+    .input
+      p tempo
+      input(type="range")#tempo
+    .input
+      p valence
+      input(type="range")#valence
+    .buttons
+      button(@click="redirect") Voltar
+      button(type="reset") Resetar
+      button(type="submit") Gerar
+</template>
+
+<script>
+export default {
+  methods: {
+    redirect() {
+      this.$router.push('playlists')
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+#customizer {
+  padding: 40px 60px;
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    .input {
+      width: 80%;
+      > input {
+        width: 100%;
+      }
+    }
+    .buttons {
+      display: flex;
+      margin-top: 10px;
+    }
+  }
+}
+</style>
--- src/scripts/router/index.js
+++ src/scripts/router/index.js
@@ -3,14 +3,16 @@ import Router from 'vue-router'
 import Index from 'PAGES/Index'
 import Playlists from 'PAGES/Playlists'
 import Playlist from 'PAGES/Playlist'
+import Customizer from 'PAGES/Customizer'
 
 Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   routes: [
-    { path: '/', name: 'Index', component: Index },
+    { path: '/', name: 'index', component: Index },
     { path: '/playlists', name: 'playlists', component: Playlists },
     { path: '/playlist/:name', name: 'playlist', component: Playlist },
+    { path: '/customizer', name: 'customizer', component: Customizer },
   ]
 })
