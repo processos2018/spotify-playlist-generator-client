8c00cccfac8c60584dae3558d184da5c3af38eed
Giordano Rodrigues <giordano2dcc@gmail.com>
2018-06-14 12:21:24 -0300

    Criação da pagina de playlists

---
+++ src/components/pages/Playlists.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+section#playlists
+  h1 Geradores de playlists pré definidos
+  .playlists
+    .playlist
+      router-link(:to="{ name: 'playlist', params: { name: 'playlist-classical' }}")
+        .image Clássica
+    .playlist
+      router-link(:to="{ name: 'playlist', params: { name: 'playlist-pop' }}")
+        .image Pop
+    .playlist
+      router-link(:to="{ name: 'playlist', params: { name: 'playlist-punk-rock' }}")
+        .image Punk Rock
+  h1 Ou se preferir gere uma playlist com seus próprios parametros >#[router-link(:to="{ name: 'customizer'}") Aqui]<.
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss">
+#playlists {
+  padding: 60px 20px;
+  > .playlists {
+    display: flex;
+    justify-content: center;
+    margin-top: 60px;
+    > .playlist {
+      margin: 0 10px;
+      margin-bottom: 10px;
+      .image {
+        background-color: white;
+        width: 200px;
+        height: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: black;
+        font-weight: bold;
+        &:hover {
+          opacity: 0.8;
+        }
+      }
+    }
+  }
+}
+</style>
--- src/scripts/router/index.js
+++ src/scripts/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Index from 'PAGES/Index'
+import Playlists from 'PAGES/Playlists'
 
 Vue.use(Router)
 
@@ -8,5 +9,6 @@ export default new Router({
   mode: 'history',
   routes: [
     { path: '/', name: 'Index', component: Index },
+    { path: '/playlists', name: 'playlists', component: Playlists },
   ]
 })
